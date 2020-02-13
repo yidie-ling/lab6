339b89246ef9e57a5c62623afecf320a637304e2
yidie-ling <57084794+yidie-ling@users.noreply.github.com>
2020-02-13 11:28:46 -0800

    lab6

--- public/js/introHCI.js
+++ public/js/introHCI.js
@@ -5,6 +5,10 @@ $(document).ready(function() {
 	initializePage();
 })
 
+$(".project").click(function(e){
+	e.preventDefault();
+});
+
 /*
  * Function that is called when the document is ready.
  */
@@ -17,6 +21,14 @@ function initializePage() {
 /*
  * Make an AJAX call to retrieve project details and add it in
  */
+
+function callBackFn(result){
+ 	console.log(result);
+ 	var projectHTML = '<img src="' + result["image"] + '" class="detailsImage" >' + "<h5>" + result["title"] + 
+	"</h5> <h6>" + result["date"] + "</h6> <p>" + result["summary"] + "</p>";
+ 	$("#project" + result["id"] + " .details").html(projectHTML);
+ }
+
 function addProjectDetails(e) {
 	// Prevent following the link
 	e.preventDefault();
@@ -27,4 +39,6 @@ function addProjectDetails(e) {
 	var idNumber = projectID.substr('project'.length);
 
 	console.log("User clicked on project " + idNumber);
+
+	$.get("/project/" + idNumber, callBackFn);
 }
